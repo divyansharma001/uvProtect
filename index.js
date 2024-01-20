a41d4f5e9827190b18f6244378b135fb35e562de
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-01-21 03:03:57 +0530

    initialCommit

---
+++ index.js
@@ -0,0 +1,199 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+import 'dotenv/config'
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const openCageAPI = process.env.OPEN_CAGE_KEY;
+const yourApiKey = process.env.OPEN_UV_KEY;
+var place;
+
+let lat = null;
+let lng = null;
+var uvEjsZero;
+var uvEjsOne;
+var uvEjsTwo;
+var uvEjsThree;
+var uvEjsFour;
+var uvEjsFive;
+var uvEjsSix;
+var uvEjsSeven;
+var uvEjsEight;
+var uvEjsNine;
+var uvEjsTen;
+
+var dateZero;
+var dateOne;
+var dateTwo;
+var dateThree;
+var dateFour;
+var dateFive;
+var dateSix;
+var dateSeven;
+var dateEight;
+var dateNine;
+var dateTen;
+
+var cityEjs;
+var stateEjs;
+var countryEjs;
+
+
+var formattedTimeZero;
+var formattedTimeOne;
+var formattedTimeTwo;
+var formattedTimeThree;
+var formattedTimeFour;
+var formattedTimeFive;
+var formattedTimeSix;
+var formattedTimeSeven;
+var formattedTimeEight;
+var formattedTimeNine;
+var formattedTimeTen;
+
+
+app.get('/', (req,res)=>{
+  res.render('index', {
+    city : cityEjs,
+    state : stateEjs,
+    country : countryEjs,
+    uvZero : uvEjsZero,
+    uvOne : uvEjsOne,
+    uvTwo : uvEjsTwo,
+    uvThree : uvEjsThree,
+    uvFour : uvEjsFour,
+    uvFive : uvEjsFive,
+    uvSix : uvEjsSix,
+    uvSeven : uvEjsSeven,
+    uvEight : uvEjsEight,
+    uvNine : uvEjsNine,
+    uvTen : uvEjsTen,
+    timeZero : formattedTimeZero,
+    timeOne : formattedTimeOne,
+    timeTwo : formattedTimeTwo,
+    timeThree: formattedTimeThree,
+    timeFour : formattedTimeFour,
+    timeFive : formattedTimeFive,
+    timeSix : formattedTimeSix,
+    timeSeven : formattedTimeSeven,
+    timeEight : formattedTimeEight,
+    timeNine : formattedTimeNine,
+    timeTen : formattedTimeTen,
+  }
+  );
+})
+
+
+
+        
+
+
+app.post('/', async (req, res) => {
+  const { latitude, longitude } = req.body.user;
+
+  if (latitude !== undefined && longitude !== undefined && !isNaN(latitude) && !isNaN(longitude)) {
+    lat = Math.round(latitude*100)/100;
+    lng = Math.round(longitude*100)/100;
+
+    console.log('Received latitude:', lat);
+    console.log('Received longitude:', lng);
+
+    try {
+      const geocodingResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=${openCageAPI}`);
+      cityEjs = geocodingResponse.data.results[0].components.city;
+      stateEjs = geocodingResponse.data.results[0].components.state;
+      countryEjs = geocodingResponse.data.results[0].components.country;
+    } catch (error) {
+      console.error('Error fetching geocoding data:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    const alt = '100';
+    const dateobj = new Date();
+    const dt = dateobj.toISOString();
+    
+
+    if (lat !== null && lng !== null) {
+      try {
+        const uvResponse = await axios.get(
+          `https://api.openuv.io/api/v1/forecast?lat=${latitude}&lng=${longitude}&alt=${alt}&dt=${dt}`,
+          {
+            headers: {
+              'x-access-token': yourApiKey,
+            },
+          }
+        );
+
+        uvEjsZero = uvResponse.data.result[0].uv;
+        uvEjsOne = uvResponse.data.result[1].uv;
+        uvEjsTwo = uvResponse.data.result[2].uv;
+        uvEjsThree = uvResponse.data.result[3].uv;
+        uvEjsFour = uvResponse.data.result[4].uv;
+        uvEjsFive = uvResponse.data.result[5].uv;
+        uvEjsSix = uvResponse.data.result[6].uv;
+        uvEjsSeven = uvResponse.data.result[7].uv;
+        uvEjsEight = uvResponse.data.result[8].uv;
+        uvEjsNine = uvResponse.data.result[9].uv;
+        uvEjsTen = uvResponse.data.result[10].uv;
+
+      console.log(stateEjs);
+
+
+     dateZero = new Date(uvResponse.data.result[0].uv_time);
+     formattedTimeZero = `${dateZero.getHours().toString().padStart(2, '0')}:${dateZero.getMinutes().toString().padStart(2, '0')}`;
+
+     dateOne = new Date(uvResponse.data.result[1].uv_time);
+     formattedTimeOne = `${dateOne.getHours().toString().padStart(2, '0')}:${dateOne.getMinutes().toString().padStart(2, '0')}`;
+
+     dateTwo= new Date(uvResponse.data.result[2].uv_time);
+     formattedTimeTwo = `${dateTwo.getHours().toString().padStart(2, '0')}:${dateTwo.getMinutes().toString().padStart(2, '0')}`;
+
+     dateThree = new Date(uvResponse.data.result[3].uv_time);
+     formattedTimeThree = `${dateThree.getHours().toString().padStart(2, '0')}:${dateThree.getMinutes().toString().padStart(2, '0')}`;
+
+     dateFour = new Date(uvResponse.data.result[4].uv_time);
+     formattedTimeFour = `${dateFour.getHours().toString().padStart(2, '0')}:${dateFour.getMinutes().toString().padStart(2, '0')}`;
+
+     dateFive = new Date(uvResponse.data.result[5].uv_time);
+     formattedTimeFive = `${dateFive.getHours().toString().padStart(2, '0')}:${dateFive.getMinutes().toString().padStart(2, '0')}`;
+
+     dateSix = new Date(uvResponse.data.result[6].uv_time);
+     formattedTimeSix = `${dateSix.getHours().toString().padStart(2, '0')}:${dateSix.getMinutes().toString().padStart(2, '0')}`;
+
+     dateSeven = new Date(uvResponse.data.result[7].uv_time);
+     formattedTimeZero = `${dateZero.getHours().toString().padStart(2, '0')}:${dateSeven.getMinutes().toString().padStart(2, '0')}`;
+
+     dateEight = new Date(uvResponse.data.result[8].uv_time);
+     formattedTimeEight = `${dateEight.getHours().toString().padStart(2, '0')}:${dateEight.getMinutes().toString().padStart(2, '0')}`;
+
+     dateNine = new Date(uvResponse.data.result[9].uv_time);
+     formattedTimeNine = `${dateNine.getHours().toString().padStart(2, '0')}:${dateNine.getMinutes().toString().padStart(2, '0')}`;
+
+     dateTen = new Date(uvResponse.data.result[10].uv_time);
+     formattedTimeTen = `${dateTen.getHours().toString().padStart(2, '0')}:${dateTen.getMinutes().toString().padStart(2, '0')}`;
+
+
+
+      } catch (error) {
+        res.status(404).send(error.message);
+        return;
+      }
+    } else {
+      res.status(400).send('Invalid latitude or longitude');
+      return;
+    }
+
+    res.send('Location received by the server');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
